52d920f191b8ab9d8d7e11c25a8da0cb3bf5c88c
Michael Baxter <msb@Michaels-MBP.localdomain>
2017-11-15 15:23:04 -0600

    Fizz Buzz Exercise

---
+++ FizzBuzz.py
@@ -0,0 +1,32 @@
+import FizzBuzzFunctions as fb
+
+
+number = int(raw_input("Please enter your number. "))
+
+fb.checkForFizzBuzz(number)
+
+#if fb.no3:
+    #print("Fizz")
+
+
+#if fb.no5:
+#    print("Buzz")
+
+
+
+
+
+
+
+
+# if number%3 == 0 and number%5 !=0:
+    #print("Buzz")
+
+#elif number%5 == 0 and number%3 !=0:
+#    print("Buzz")
+
+#elif number%3 == 0 and number%5 == 0:
+#    print("FizzBuzz")
+
+#elif number%3 != 0 and number%5 != 0:
+#    print("NOPE!")
---
+++ FizzBuzzFunctions.py
@@ -0,0 +1,8 @@
+
+def checkForFizzBuzz(no):
+    if no % 3 == 0 and no % 5 == 0:
+        print("Fizz Buzz")
+    elif no % 3 == 0:
+        print("Fizz")
+    elif no % 5 == 0:
+        print("Buzz")
